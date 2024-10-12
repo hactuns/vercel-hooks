788686852c15c7971c9779b2135a47862205b750
TuanTran <khactuans.tran@gmail.com>
2024-10-12 23:13:53 +0700

    feat: update handler typing

--- src/api-handler/index.ts
+++ src/api-handler/index.ts
@@ -1,22 +1,51 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+import type { Request, Response } from 'express';
+import withMongo from '../mongo/withMongo.js';
 
-export type APIHandler = (
-  req: VercelRequest,
-  res: VercelResponse
-) => Promise<VercelResponse> | VercelResponse;
+export interface RequestContext<
+  Req extends Request | VercelRequest = Request,
+  Res extends Response | VercelResponse = Response,
+> {
+  req: Req;
+  res: Res;
+}
 
-export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+export type APIHandler<RC extends RequestContext = RequestContext, Context = object> = (
+  req: RC['req'],
+  res: RC['res'],
+  context: Context
+) => Promise<RC['res']> | RC['res'];
+
+export enum HttpMethod {
+  GET = 'GET',
+  POST = 'POST',
+  PUT = 'PUT',
+  PATCH = 'PATCH',
+  DELETE = 'DELETE',
+}
+
+export type HandlerFunction<
+  Key extends HttpMethod,
+  Req extends Request | VercelRequest = Request,
+  Res extends Response | VercelResponse = Response,
+> = Record<Key, RequestContext<Req, Res>>;
+
+type HandlerCallback<RC extends Record<string, RequestContext>> = {
+  [Method in keyof RC]: APIHandler<RC[Method], object>;
+};
 
 export const handler =
-  (callback: Partial<Record<HttpMethod, APIHandler>>) =>
-  (req: VercelRequest, res: VercelResponse) => {
-    const method = req.method?.toUpperCase() as HttpMethod;
+  <Method extends HttpMethod, HandlerType extends Record<Method, HandlerFunction<Method>[Method]>>(
+    callback: HandlerCallback<HandlerType>
+  ) =>
+  (req: HandlerType[Method]['req'], res: HandlerType[Method]['res']) => {
+    const method = req.method?.toUpperCase() as keyof typeof callback;
 
     if (typeof callback[method] !== 'function') {
-      return res.json({ message: 'Method not found' });
+      return res.status(405).json({ message: 'Method not allowed' });
     }
 
-    return callback[method](req, res);
+    return callback[method](req, res, {});
   };
 
 export default handler;
--- src/mongo/withMongo.ts
+++ src/mongo/withMongo.ts
@@ -1,28 +1,19 @@
 import { MongoClient } from 'mongodb';
 import { Mongo } from './mongoClient.js';
-
-export type ApiResponse<T = Response> = T | Response | undefined;
-export type ApiRequest<T = Request> = T | Request | undefined;
+import type { Response, Request } from 'express';
+import type { APIHandler, RequestContext } from '../api-handler/index.js';
 
 export const withMongo =
-  <REQ = ApiRequest, RES = ApiResponse>(
-    callback: (
-      req: ApiRequest | REQ,
-      res: ApiResponse | RES,
-      db: MongoClient
-    ) => Promise<ApiResponse | RES> | ApiResponse | RES
-  ) =>
-  async (req: ApiRequest | REQ, res: ApiResponse | RES): Promise<ApiResponse | RES> => {
+  <Req extends Request = Request, Res extends Response = Response, Context extends object = object>(
+    callback: APIHandler<RequestContext<Req, Res>, Record<'client', MongoClient> & Context>
+  ): APIHandler<RequestContext<Req, Res>, Context> =>
+  async (req, res, context) => {
     try {
       const client = await Mongo.getClient().connect();
 
-      console.log('[MONGO_INSTANCE]: connected');
-
-      return callback(req, res, client);
+      return callback(req, res, { ...context, client });
     } catch (error) {
-      return new Response(JSON.stringify({ message: 'Can not connect to mongo instance' }), {
-        status: 400,
-      });
+      return res.json({ message: 'Can not connect to mongo instance' });
     }
   };
 
